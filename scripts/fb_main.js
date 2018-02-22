c2d4cdb27ca08040a16887ba95bb49c15a12010e
amelia15 <rina.sl@ukr.net>
2018-02-22 20:55:46 +0200

    Update fb_main.js

--- scripts/fb_main.js
+++ scripts/fb_main.js
@@ -3,29 +3,16 @@
   
     //получим нужный нам список в бд (по его ключу)
     var listRef = db.ref('main');
-   
 
    //добавление элемента в отображение
     function addItem(item) {
-	
         //cоздадим новый элемент списка с данными из аргумента (каждый item состоит из ключа и значения)
         var newItem = '<div class="general_facts"><div class="name" onclick="show(this)"> <h2>'+item.val().name+' </h2></div><div class="text_info">'+item.val().text+' </div></div>';
         //добавление элемента списка в список
         document.querySelector('.info').innerHTML += newItem;
     }
-	
-	
-    //изменение отображения элемента 
-    function changeItem(item) {
-        //получили измененный элемент
-        var element = document.querySelector('#list li[name="'+item.key+'"]');
-        //удалили его предыдущее отображение
-        element.parentElement.removeChild(element);
-        //добавили заново
-        addItem(item);
-    }
-    
-	//добавление "слушателей" событий
+		
+    	//добавление "слушателей" событий
     //если в бд что-то добавили
     listRef.on('child_added', function (data) {
         addItem(data);
